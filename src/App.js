9e5669da61ec6c12cef91bbb469b6820bbb5338c
skriukaite <skriukaite@gmail.com>
2022-01-31 10:33:01 +0200

    Add basic html structure of the App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Weather />;
+      <div className="container">
+        <header> Weather App</header>
+        <Weather />;
+        <footer>
+          {" "}
+          This project was coded by Sandra Kriukaitė and is open-sourced on{" "}
+          <a
+            href="https://github.com/skriukaite/weather-search-project"
+            taget="_blank"
+          >
+            {" "}
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
