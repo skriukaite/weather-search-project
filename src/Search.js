323873f1caa55595b4f0db8ac5e2efda17de95df
skriukaite <skriukaite@gmail.com>
2022-02-01 10:42:19 +0200

    Add basic styling

--- src/Search.js
+++ src/Search.js
@@ -3,9 +3,13 @@ import "./Search.css";
 
 export default function Search() {
   return (
-    <form className="Search">
-      <input type="text" placeholder="Enter a city"></input>
-      <input type="submit" value="search"></input>
+    <form className="Search form-inline form-group">
+      <input
+        type="text"
+        placeholder="Enter a city"
+        className="form-control input-line"
+      ></input>
+      <input type="submit" value="Search" className="btn btn-secondary"></input>
     </form>
   );
 }
