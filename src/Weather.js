a223af83f3df0ff88d319466c36d1f9b554fdf0f
skriukaite <skriukaite@gmail.com>
2022-02-01 11:33:45 +0200

    Add axios to the App

--- src/Weather.js
+++ src/Weather.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="weatherProperties">
-        <h1>New York </h1>
-        <ul>
-          <li>Wednesday 07:00</li>
-          <li>Mostly cloudy</li>
-        </ul>
-      </div>
-      <div className="temperatureColumn">
-        <div className="currentTemperature">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
-            alt="Rain"
-          />
-          <div>
-            7 <span className="units">C°|F°</span>{" "}
+  const [temperature, setTemperature] = useState(null);
+  const [ready, setReady] = useState(false);
+
+  function getResponse(response) {
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <div className="weatherProperties">
+          <h1>New York </h1>
+          <ul>
+            <li>Wednesday 07:00</li>
+            <li>Mostly cloudy</li>
+          </ul>
+        </div>
+        <div className="temperatureColumn">
+          <div className="currentTemperature">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
+              alt="Rain"
+            />
+            <div>
+              {temperature} <span className="units">C°|F°</span>{" "}
+            </div>
+          </div>
+          <div className="temperatureProperties">
+            <div>Precipitation: 90%</div>
+            <div>Humidity: 94%</div>
+            <div>Wind: 5 m/s</div>
           </div>
         </div>
-        <div className="temperatureProperties">
-          <div>Precipitation: 90%</div>
-          <div>Humidity: 94%</div>
-          <div>Wind: 5 m/s</div>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "c9e949520b33b479d97265ad4b8693ed";
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(getResponse);
+    return "Loading";
+  }
 }
